ec7b93544dfcf9aed5066131386cdca2849d0c9e
Denys <densid06@gmail.com>
2025-03-25 22:08:21 +0200

    Work Started Added work with file Added basic stdout

--- nma.asm
+++ nma.asm
@@ -1,18 +1,51 @@
-.model tiny
-.code
-org 100h
-start:
-    call print_message
+.MODEL small
+.STACK 100h
+.DATA
+    buffer db 32768 dup(?)     ; дані/буфер
+    fileName db "input.nma", 0 ; файл який будемо читати
+    fileHandle dw ?            ; handle
     
-    mov ax, 4C00h
+    bytesRead dw ?             ; скільки прочитали
+.CODE
+main:
+    mov ax, @data
+    mov ds, ax
+
+    ; відкриваємо файл
+    mov ah, 3Dh        
+    mov al, 0          ; читати
+    lea dx, fileName   ; пихаєм файл
+    int 21h
+    mov fileHandle, ax ; наш дескриптор
+
+    ; читаємо файл
+    mov ah, 3Fh        ; в бх пихаєм дескриптор, дх що читать, сх скільки будемо читати
+    mov bx, fileHandle     
+    lea dx, buffer     
+    mov cx, 32768      ; читаемо до 32768 байт
+    int 21h
+    mov bytesRead, ax  ; скільки прочитали
+
+    ; закриваємо файл
+    mov ah, 3Eh        ; закрить файл
+    mov bx, fileHandle
+    int 21h
+;|||||||||||||||||||||||тут буде работа з даними файлу, які лежать у буфері, поки що з цікавого тут просто вивод||||||||||||||||||||||||||
+
+
+
+
+
+
+
+stdout:
+    mov ah, 40h
+    mov bx, 1
+    mov cx, bytesRead ; скільки байт вивести
+    lea dx, buffer ; виводимо буфер
     int 21h
 
-print_message proc
-    mov dx, offset msg
-    mov ah, 09h
+ende:
+    mov ah, 4Ch        
     int 21h
-    ret
-print_message endp
-    
-msg db '24', 0Dh, 0Ah, '$'
-end start
+END main
